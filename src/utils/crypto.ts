04c2b886b2a8f974e00118e2f5d2c9a6fc2755a9
shi-lai-mu <478889187@qq.com>
2020-11-29 08:19:37 +0800

    feat: 添加工具类hash生成方法

---
+++ src/utils/crypto.ts
@@ -0,0 +1,17 @@
+import {
+  createHash
+} from 'crypto';
+
+
+/**
+ * 生成hash
+ * @param data     数据
+ * @param hashType HASH生成方式
+ * @param digest   摘要方式
+ * @param isUpper  是否转为大写
+ */
+export function generateHash(data: any, hashType: 'md5' | 'sha1' | 'sha256' | 'sha512' = 'md5', digest: any = 'hex', isUpper: boolean = true): string {
+  let hash = createHash(hashType).update(data).digest(digest);
+  isUpper && (hash = hash.toLocaleUpperCase());
+  return hash;
+}
